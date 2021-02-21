60c948a25a79d04d4a094d5402a4fcb2ed48d407
Aleksei.Polianskii <aleksei.polianskii@mail.ru>
2021-02-21 23:02:27 +0700

    Работа с шаблонами, не смог пока настроить вывод в products цену и описание из context

--- geekshop/urls.py
+++ geekshop/urls.py
@@ -16,10 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from mainapp.views import index, prodacts
+from mainapp.views import index, prodacts, test_context
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index),
     path('products/', prodacts),
+    path('test/', test_context)
 ]
