711446a8c5a78bef8b59346c52936140088f5f4e
Aleksei.Polianskii <aleksei.polianskii@mail.ru>
2021-03-01 00:44:07 +0700

    выполнил все задания, но есть проблема при загрузке данных из json в БД

--- geekshop/settings.py
+++ geekshop/settings.py
@@ -122,3 +122,8 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+FIXTURE_DIRS = ['mainapp/fixture']
