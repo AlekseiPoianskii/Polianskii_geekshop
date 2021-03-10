3f381b0f3035b361d9aff36429b31d3e352b8bfa
Aleksei.Polianskii <aleksei.polianskii@mail.ru>
2021-03-11 00:02:50 +0700

    добавил выход и доработал страницы

--- authapp/forms.py
+++ authapp/forms.py
@@ -9,10 +9,33 @@ class UserLoginForm(AuthenticationForm):
         fields = ('username', 'password')
 
     def __init__(self, *args, **kwargs):
-        super(UserLoginForm,self).__init__(*args, **kwargs)
+        super(UserLoginForm, self).__init__(*args, **kwargs)
         self.fields['username'].widget.attrs['placeholder'] = 'Введите имя пользователя'
         self.fields['password'].widget.attrs['placeholder'] = 'Введите пароль'
         self.fields['username'].widget.attrs['type'] = 'email'
         self.fields['password'].widget.attrs['type'] = 'password'
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'form-control py-4'
+
+
+class UserRegisterForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'first_name', 'last_name', 'password1', 'password2')
+
+    def __init__(self, *args, **kwargs):
+        super(UserRegisterForm, self).__init__(*args, **kwargs)
+        self.fields['username'].widget.attrs['placeholder'] = 'Введите имя пользователя'
+        self.fields['email'].widget.attrs['placeholder'] = 'Введите адрес эл. почты'
+        self.fields['first_name'].widget.attrs['placeholder'] = 'Введите имя'
+        self.fields['last_name'].widget.attrs['placeholder'] = 'Введите фамилию'
+        self.fields['password1'].widget.attrs['placeholder'] = 'Введите парольь'
+        self.fields['password2'].widget.attrs['placeholder'] = 'Подтвердите пароль'
+        self.fields['username'].widget.attrs['type'] = 'text'
+        self.fields['email'].widget.attrs['type'] = 'email'
+        self.fields['first_name'].widget.attrs['type'] = 'text'
+        self.fields['last_name'].widget.attrs['type'] = 'text'
+        self.fields['password1'].widget.attrs['type'] = 'password'
+        self.fields['password2'].widget.attrs['type'] = 'password'
         for field_name, field in self.fields.items():
             field.widget.attrs['class'] = 'form-control py-4'
--- authapp/views.py
+++ authapp/views.py
@@ -3,7 +3,8 @@ from django.contrib import auth
 from django.urls import reverse
 from django.contrib import messages
 
-from authapp.forms import UserLoginForm, UserCreationForm
+from authapp.forms import UserLoginForm, UserRegisterForm
+
 
 def login(request):
     if request.method == 'POST':
@@ -22,5 +23,17 @@ def login(request):
 
 
 def register(request):
-    return render(request, 'authapp/register.html')
+    if request.method == 'POST':
+        form = UserRegisterForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('auth:login'))
+    else:
+        form = UserRegisterForm()
+    context = {'form': form}
+    return render(request, 'authapp/register.html', context)
 
+
+def logout(request):
+    auth.logout(request)
+    return HttpResponseRedirect(reverse('index'))
