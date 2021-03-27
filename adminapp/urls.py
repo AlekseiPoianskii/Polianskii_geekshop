f071f94c08d6f6fdfa4e477e482cb5a94db6695b
Aleksei.Polianskii <aleksei.polianskii@mail.ru>
2021-03-27 17:32:30 +0700

    проработал изменение и удаление профиля

--- adminapp/urls.py
+++ adminapp/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path
 
-from adminapp.views import index, admin_users, admin_users_create, admin_users_update, admin_users_delete
+from adminapp.views import index, admin_users, admin_users_create, admin_users_update, admin_users_delete,admin_users_rebuild
 
 app_name = 'adminapp'
 
 urlpatterns = [
     path('', index, name='index'),
     path('users/', admin_users, name='admin_users'),
-    path('users-create/', admin_users_create, name='admin_users_create'),
-    path('users-update/', admin_users_update, name='admin_users_update'),
+    path('user-create/', admin_users_create, name='admin_users_create'),
+    path('user-update/<int:user_id>/', admin_users_update, name='admin_users_update'),
+    path('user-delete/<int:user_id>/', admin_users_delete, name='admin_users_delete'),
+    path('user-rebuild/<int:user_id>/', admin_users_rebuild, name='admin_users_rebuild'),
 ]
--- adminapp/views.py
+++ adminapp/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, HttpResponseRedirect
 from django.urls import reverse
 
 from authapp.models import User
-from adminapp.forms import UserAdminRegisterForm
+from adminapp.forms import UserAdminRegisterForm, UserAdminProfileForm
 
 
 def index(request):
@@ -22,13 +22,32 @@ def admin_users_create(request):
             return HttpResponseRedirect(reverse('admin_staff:admin_users'))
     else:
         form = UserAdminRegisterForm()
-    context = {'form': form,}
+    context = {'form': form}
     return render(request, 'adminapp/admin-users-create.html', context)
 
 
-def admin_users_update(request):
-    return render(request, 'adminapp/admin-users-update-delete.html')
+def admin_users_update(request, user_id=None):
+    user = User.objects.get(id=user_id)
+    if request.method == 'POST':
+        form = UserAdminProfileForm(data=request.POST, files=request.FILES, instance=user)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('admin_staff:admin_users'))
+    else:
+        form = UserAdminProfileForm(instance=user)
+    context = {'form': form,
+               'user': user}
+    return render(request, 'adminapp/admin-users-update-delete.html', context)
 
 
-def admin_users_delete(request):
-    pass
+def admin_users_delete(request, user_id=None):
+    user = User.objects.get(id=user_id)
+    user.is_active = False
+    user.save()
+    return HttpResponseRedirect(reverse('admin_staff:admin_users'))
+
+def admin_users_rebuild(request, user_id=None):
+    user = User.objects.get(id=user_id)
+    user.is_active = True
+    user.save()
+    return HttpResponseRedirect(reverse('admin_staff:admin_users'))
